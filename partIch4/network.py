d0d9473f9fd0cde67927d5989a40ee9ae029197f
nobodypengium <nobodypengium@gmail.com>
2019-04-13 12:14:53 +0800

    Update PartI ch4

--- partIch4/network.py
+++ partIch4/network.py
@@ -5,19 +5,33 @@ import partIch4.testCases
 from partIch4.dnn_utils import *
 import partIch4.lr_utils
 
+
 # %% 初始化两层网络
 def initialize_parameters(n_x, n_h, n_y):
-    W1 = np.random.randn(n_h,n_x)*0.01
-    b1 = np.zeros((n_h,1))
-    W2 = np.random.randn(n_y,n_h)*0.01
-    b2 = np.zeros((n_y,1))
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
 
     parameters = {
-        "W1":W1,
-        "b1":b1,
-        "W2":W2,
-        "b2":b2
+        "W1": W1,
+        "b1": b1,
+        "W2": W2,
+        "b2": b2
     }
 
     return parameters
 
+
+# %% 对于深度的神经网络的节点初始化
+def initialize_parameters_deep(layers_dims):  # layers_dims秩为1数组，layers_dims[l]包含第l层节点的数量
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1, L):  # 第0层不用初始化，那是输入
+        parameters["W" + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) / np.sqrt(
+            layers_dims[l - 1])  # 用除以平方根代替防止梯度消失或梯度爆炸
+        parameters["b" + str(l)] = np.zeros((layers_dims[l], 1))#zeros写法的诡异错误，要套两层括号
+
+    return parameters
--- partIch4/test.py
+++ partIch4/test.py
@@ -6,6 +6,7 @@ from partIch4.dnn_utils import *
 from partIch4.lr_utils import *
 from partIch4.network import *
 
+# %% 输入数据
 np.random.seed(1)
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
@@ -13,13 +14,23 @@ train_set_x = train_set_x_flatten / 255
 test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
 test_set_x = test_set_x_flatten / 255
 
-n_x = train_set_x[0]
+n_x = train_set_x.shape[0]
 n_h = 4
-n_y = train_set_y[0]
+n_y = train_set_y.shape[0]
 
+# %% 测试初始化两层神经网络
 parameters = initialize_parameters(n_x, n_h, n_y)
 
 print("W1: " + str(parameters["W1"].shape))
 print("b1: " + str(parameters["b1"].shape))
 print("W2: " + str(parameters["W2"].shape))
 print("b2: " + str(parameters["b2"].shape))
+
+# %% 测试初始化深层神经网络
+
+layers_dims = [n_x, 4, 3, n_y]
+parameters_deep = initialize_parameters_deep(layers_dims)
+for l in range(1, layers_dims.__len__()):
+    print("W" + str(l) + ": " + str(parameters_deep["W" + str(l)].shape))
+    print("b" + str(l) + ": " + str(parameters_deep["b" + str(l)].shape))
+
